e9a9582ffec900afcc1fcf39293e4aa868b2b25c
con40 <andrew.conforti@gmail.com>
2020-12-21 13:58:03 -0500

    Update index.js
    disable non auth0 session

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(
-  session({
-   secret: SESSION_SECRET,
-   authRequired: false,
-  resave: false,
-    saveUninitialized: true,
-  })
-);
+//app.use(
+//  session({
+ //  secret: SESSION_SECRET,
+//   authRequired: false,
+//  resave: false,
+//    saveUninitialized: true,
+//  })
+//);
 
 // 👉 Replace this with auth middleware 👈
 
